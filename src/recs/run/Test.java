898477a85231341d53e7c80622a2887a53ec3f54
Filip <fil.rehburg@gmail.com>
2022-04-06 19:36:55 +0200

    Added recommendation codebase

---
+++ src/recs/run/Test.java
@@ -0,0 +1,13 @@
+package run;
+
+import split.LudiiFileCleanup;
+import split.SentenceSplit;
+
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        String w = "Hallo .//-------";
+        System.out.println(w = w.substring(0,w.lastIndexOf("//")));
+    }
+}
